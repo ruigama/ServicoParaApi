5bcfde0559c4e6a9955d8925c361473b9c6fa34a
Rui Gama <rocknagama@live.com.pt>
2024-01-19 16:38:07 -0300

    Adicionar arquivos de projeto.

---
+++ ServicoParaApi/MyServiceInstaller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+[RunInstaller(true)]
+public class MyServiceInstaller : Installer
+{
+    private ServiceProcessInstaller processInstaller;
+    private ServiceInstaller serviceInstaller;
+
+    public MyServiceInstaller()
+    {
+        // Configurar o instalador do processo do serviço
+        processInstaller = new ServiceProcessInstaller();
+        processInstaller.Account = ServiceAccount.LocalSystem;
+
+        // Configurar o instalador do serviço
+        serviceInstaller = new ServiceInstaller();
+        serviceInstaller.StartType = ServiceStartMode.Automatic;
+        serviceInstaller.ServiceName = "ServicoApi"; // Substitua pelo nome do seu serviço
+        serviceInstaller.Description = "Serviço de chamada de API"; // Substitua pela descrição do seu serviço
+
+        // Adicionar instaladores ao instalador principal
+        Installers.Add(processInstaller);
+        Installers.Add(serviceInstaller);
+    }
+}
---
+++ ServicoParaApi/Service1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace ServicoParaApi
+{
+    partial class Service1
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Service1
+            // 
+            this.ServiceName = "Service1";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
